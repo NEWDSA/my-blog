5591d4d44d2342bf990d6e410ad246a0dcfd070a
NEWDAS <953627561@qq.com>
2020-06-15 12:07:11 +0800

    提交更新

---
+++ my-blog/src/api/addimg.js
@@ -0,0 +1,14 @@
+import request from '@/utils/request'
+
+export default {
+  upimg (title,data) {
+    return request({
+      url: '/img',
+      method: 'get',
+      data:{
+        title:title,
+        content:data
+      }
+    })
+  }
+}
